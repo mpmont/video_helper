8c88168ed30fb381603655feb042eb2829c7a91a
Marco Monteiro <mpmont@gmail.com>
2012-07-15 14:12:38 +0100

    added autoload & changed version

---
+++ config/autoload.php
@@ -0,0 +1,9 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ *
+ * @author              Marco Monteiro <marco@marcomonteiro.net>
+ * @license		        http://www.opensource.org/licenses/MIT
+ */
+
+$autoload['helper'] = array('video');
